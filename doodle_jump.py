05dce4b23c6621739ff1ecacb8bcb1965983fe9a
thermodynathan <sh4aj@virginia.com>
2018-11-16 00:13:31 -0500

    Starting point

---
+++ doodle_jump.py
@@ -0,0 +1,23 @@
+# Written by Simeng Hao (sh4aj) and Derek Habron (hjb5ek)
+
+import pygame
+import gamebox
+camera = gamebox.Camera(400,600)
+
+# Key attributes
+
+
+
+
+# Definitions
+
+
+
+
+# Mainloop
+def tick(keys):
+    pass
+
+
+# Initiation
+gamebox.timer_loop(60,tick)
